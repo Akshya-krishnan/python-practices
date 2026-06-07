0c8a6699ee510c08080cde83adafa49a35328a1b
Akshaya <akshayakrishnan4147@gmail.com>
2026-06-07 17:58:59 +0530

    Type Casting

--- python.py
+++ python.py
@@ -1,6 +1,8 @@
-print("Hello world")
-print("iam learning python")
-a = 10
-b = 20
-c= a+b
-print(c)
+x=10
+y=10.90
+z="Akshaya"
+a='comstream'
+
+add_no, stud_name, age, is_passed = (101,"Akshaya",23,True)
+
+print(x,y,z,a,add_no,stud_name,age,is_passed)
